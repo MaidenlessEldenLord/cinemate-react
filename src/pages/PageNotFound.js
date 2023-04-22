b746c267bd3f115c9dff9741a7b6ee89aa68322e
MaidenlessEldenLord <greggjamal@gmail.com>
2023-04-22 18:21:21 -0400

    Cinemate-React-Update

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,29 @@
+import PageNotFoundImage from '../assets/images/pagenotfound.jpg';
+import { Link } from 'react-router-dom';
+import { Button } from '../components';
+import { useEffect } from 'react';
+
 export const PageNotFound = () => {
+
+  useEffect(() => {
+    document.title = `Page Not Found / Cinemate`;
+  });
+
   return (
-    <main>PageNotFound</main>
+    <main>
+      <section className="flex flex-col justify-center px-2">
+        <div className="flex flex-col items-center my-4">
+          <p className='text-gray-700 text-7xl font-bold my-10 dark:text-white'>404, Oops!</p>
+          <div className='max-w-lg'>
+            <img className='rounded' src={PageNotFoundImage} alt="404 Page Not Found" />
+          </div>
+        </div>
+        <div className='flex justify-center my-4'>
+          <Link to='/'>
+            <Button>Back To Cinemate</Button>
+          </Link>
+        </div>
+      </section>
+    </main>
   )
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,14 @@
 import { Card } from '../components';
 import { useFetch } from '../hooks/useFetch';
 import { useSearchParams } from 'react-router-dom';
+import { useTitle } from '../hooks/useTitle';
 
-export const Search = ({apiPath}) => { 
+export const Search = ({apiPath, title}) => { 
   const [searchParams] = useSearchParams(); 
   const queryTerm = searchParams.get('q');
 
   const { data: movies } = useFetch(apiPath, queryTerm); 
+  useTitle(`Search result for ${queryTerm}`); 
 
   return (
     <main>
